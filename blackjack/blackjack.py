5df4315ea19c5b5f2a7300662ff68a4b8267485c
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-05-26 11:10:44 -0600

    Added helper functions to prints scores, logic when player gets 21, play again function and ASCII assets

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -13,6 +13,12 @@ def sum_numbers(cards_list):
         card_total += card
     return card_total
 
+def print_player_score(player_cards, player_score):
+    print(f'Player Cards: {" ".join(map(str, player_cards))} | Score: {player_score}')
+
+def print_computer_score(computer_cards, computer_score):
+    print(f'Computer Cards: {" ".join(map(str, computer_cards))} | Score: {computer_score}')
+
 def initialize_game(player_cards, house_cards):
     for i in range(2):
         draw_card(player_cards)
@@ -21,7 +27,7 @@ def initialize_game(player_cards, house_cards):
     player_sum = sum_numbers(player_cards)
     house_sum = sum_numbers(house_cards)
 
-    print(f'Player Cards: {" ".join(map(str, player_cards))} | Current score: {player_sum}')
+    print_player_score(player_cards, player_sum)
     print(f"House cards: {house_cards[0]}")
     return player_sum, house_sum
 
@@ -34,23 +40,36 @@ def game_logic():
     
     while current_player_score < 21:
         hit_me = input("Do you want another card? Yes or No? ").lower()
+        
         if hit_me == "yes":
             draw_card(player_cards)
             current_player_score = sum_numbers(player_cards)
-            print(f'Player Cards: {" ".join(map(str, player_cards))} | Current score: {current_player_score}')
+            print_player_score(player_cards, current_player_score)
         else:
             break
-        if current_player_score > 21:
-            print("You lose!")
-            print(f'Player Cards: {" ".join(map(str, player_cards))} | Current score: {current_player_score}')
+    
+        if current_player_score == 21:
+            print("You win!")
+            print_player_score(player_cards, current_player_score)
+            return
+
+        elif current_player_score > 21:
+            print("\nYou lose!")
+            print_player_score(player_cards, current_player_score)
+            print_computer_score(house_cards, house_score)
             return
 
     if current_player_score < house_score:
         print("House wins!")
     else:
         print("You win!")
+        print_player_score(player_cards, current_player_score)
+        print_computer_score(house_cards, house_score)
     
-    print(player_cards, house_cards, current_player_score, house_score)
+    continue_playing = input("Play again? Yes or No? ").lower()
+    if continue_playing == "yes":
+        game_logic()
+    else:
+        print("Thank you for playing!")
 
 game_logic()
-
