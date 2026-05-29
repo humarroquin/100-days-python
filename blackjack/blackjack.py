f02a3ea35ebfa5b03343bf714249b5100d22f519
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-05-29 11:16:18 -0600

    improved comparison flow and dealer now gets cards if score is below 17

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -14,12 +14,11 @@ def sum_numbers(cards_list):
         card_total += game_assets.CARD_VALUES[card]
     return card_total
 
-def print_player_score(player_cards, player_score):
-    print("Player Cards:")
+def print_cards_and_score(cards, user, score="**"):
+    print(f"{user} Cards:")
     
-    #todo: turn this into a reusable function
     all_cards = []
-    for card in player_cards:
+    for card in cards:
         split_card = game_assets.CARD_ART[card].splitlines()
         cleaned_split_card = []
         for i in split_card:
@@ -32,34 +31,7 @@ def print_player_score(player_cards, player_score):
             print(card[row], end=" ")
         print()
 
-    print(f"Score: {player_score}\n")
-
-def print_computer(computer_cards):
-    print("Computer Cards:")
- 
-    all_cards = []
-    first_card = computer_cards[0]
-    
-    # Card 1
-    split_card = game_assets.CARD_ART[first_card].splitlines()
-    cleaned_card = []
-    for i in split_card:
-            if i != "": 
-                cleaned_card.append(i)
-    all_cards.append(cleaned_card)
-
-    # Hidden card
-    card_hidden = game_assets.CARD_ART["HIDDEN"].splitlines()
-    card_cleaned_hidden = []
-    for i in card_hidden:
-            if i != "": 
-                card_cleaned_hidden.append(i)
-    all_cards.append(card_cleaned_hidden)
-
-    for row in range(3):
-        for card in all_cards:
-            print(card[row], end=" ")
-        print()
+    print(f"Score: {score}\n")
 
 def initialize_game(player_cards, house_cards):
     for i in range(2):
@@ -69,8 +41,11 @@ def initialize_game(player_cards, house_cards):
     player_sum = sum_numbers(player_cards)
     house_sum = sum_numbers(house_cards)
 
-    print_player_score(player_cards, player_sum)
-    print_computer(house_cards)
+    print_cards_and_score(player_cards, "Player", player_sum)
+
+    initial_house_display = [house_cards[0], "HIDDEN"]
+    print_cards_and_score(initial_house_display, "House")
+
     return player_sum, house_sum
 
 # === game logic starts ===
@@ -79,42 +54,72 @@ def game_logic():
     player_cards = []
     house_cards = []
 
-    current_player_score, house_score = initialize_game(player_cards, house_cards)
-    
-    while current_player_score < 21:
+    current_player_score, current_house_score = initialize_game(player_cards, house_cards)
+    game_status = ""
+
+    # player starts turn
+    active_game = True
+    while active_game:
         hit_me = input("Do you want another card? Yes or No? ").lower()
         
         if hit_me == "yes":
             draw_card(player_cards)
             current_player_score = sum_numbers(player_cards)
-            print_player_score(player_cards, current_player_score)
+
+            print_cards_and_score(player_cards, "Player", current_player_score)
+            initial_house_display = [house_cards[0], "HIDDEN"]
+            print_cards_and_score(initial_house_display, "House")
+
+            if current_player_score > 21:
+                game_status = "player_bust"
+                active_game = False
+            elif current_player_score == 21:
+                game_status = "player_wins"
+                active_game = False
+
         else:
-            break
+            active_game = False
     
-        if current_player_score == 21:
-            print(game_assets.BLACKJACK_SCREENS["win"])
-            print_player_score(player_cards, current_player_score)
-            return
-
-        elif current_player_score > 21:
-            print(game_assets.BLACKJACK_SCREENS["bust"])
-            print_player_score(player_cards, current_player_score)
-            print_computer_score(house_cards, house_score)
-            return
-
-    if current_player_score < house_score:
-        print(game_assets.BLACKJACK_SCREENS["bust"])
-    elif current_player_score == house_score:
-        print(game_assets.BLACKJACK_SCREENS["push"])
+    if game_status == "player_bust" or game_status == "player_wins":
+        print(game_assets.BLACKJACK_SCREENS[game_status])
+        print_cards_and_score(player_cards, "Player", current_player_score)
+    
     else:
-        print(game_assets.BLACKJACK_SCREENS["win"])
-        print_player_score(player_cards, current_player_score)
-        print_computer_score(house_cards, house_score)
-    
+        # dealer starts turn
+        while current_house_score < 17:
+            draw_card(house_cards)
+            current_house_score = sum_numbers(house_cards)
+            print_cards_and_score(player_cards, "Player", current_player_score)
+            print_cards_and_score(house_cards, "House", current_house_score)
+
+            if current_house_score > 21:
+                game_status = "house_bust"
+
+            elif current_house_score == 21:
+                game_status = "house_wins"
+
+        if game_status == "house_bust" or game_status == "house_wins":
+            print(game_assets.BLACKJACK_SCREENS[game_status])
+            print_cards_and_score(house_cards, "House", current_house_score)
+
+        else:
+            if current_player_score > current_house_score:
+                game_status = "player_wins"
+            elif current_player_score == current_house_score:
+                game_status = "push"
+            else:
+                game_status = "house_wins"
+
+            print(game_assets.BLACKJACK_SCREENS[game_status])
+            print_cards_and_score(player_cards, "Player", current_player_score)
+            print_cards_and_score(house_cards, "House", current_house_score)
+
     continue_playing = input("Play again? Yes or No? ").lower()
     if continue_playing == "yes":
         game_logic()
     else:
-        print("Thank you for playing!")
+        print("""> blackjack
+dealer disconnected.
+game terminated.""")
 
 game_logic()
