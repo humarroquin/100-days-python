4751bb5e6665ba1dc48774e4c67761a78e94c5d5
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-06-04 15:26:08 -0600

    programm will now loop unless user selects off and messages from the program are more descriptive

--- coffee-app/coffee.py
+++ coffee-app/coffee.py
@@ -1,33 +1,44 @@
 available_resources = {
     "water": 100,
-    "milk": 50,
-    "coffee":76,
+    "milk": 0,
+    "coffee":0,
     "money": 0
 }
 
 coffee_options = {
     "cappuccino": {"water": 30, "milk": 10, "coffee": 10, "money": 2.5},
     "latte": {"water": 30, "milk": 20, "coffee": 10, "money": 1.5},
-    "espresso": {"water": 30, "milk": 0, "coffee": 10, "money": 1.75}
+    "espresso": {"water": 300, "milk": 0, "coffee": 10, "money": 1.75}
 }
 
-def check_resources(coffee_choice, available_resources):
+def get_variables(coffee_choice, available_resources):
     water_needed = coffee_choice["water"]
     coffee_needed = coffee_choice["coffee"]
     milk_needed = coffee_choice["milk"]
     water_available = available_resources["water"]
     coffee_available = available_resources["coffee"]
     milk_available = available_resources["milk"]
+    return water_needed, coffee_needed, milk_needed, water_available, coffee_available, milk_available
 
-    return water_needed <= water_available and coffee_needed <= coffee_available and milk_needed <= milk_available
-        
+def check_resources(coffee_choice, available_resources):
+    water_needed, coffee_needed, milk_needed, water_available, coffee_available, milk_available = get_variables(coffee_choice, available_resources)
+
+    missing_ingredients = []
+    can_make_drink = True
+    if water_available < water_needed:
+        missing_ingredients.append("Not enough water")
+        can_make_drink = False
+    if coffee_available < coffee_needed:
+        missing_ingredients.append("Not enough coffee")
+        can_make_drink = False
+    if milk_available < milk_needed:
+        missing_ingredients.append("Not enough milk")
+        can_make_drink = False
+
+    return can_make_drink, missing_ingredients
+
 def update_resources(coffee_choice, available_resources):
-    water_needed = coffee_choice["water"]
-    coffee_needed = coffee_choice["coffee"]
-    milk_needed = coffee_choice["milk"]
-    water_available = available_resources["water"]
-    coffee_available = available_resources["coffee"]
-    milk_available = available_resources["milk"]
+    water_needed, coffee_needed, milk_needed, water_available, coffee_available, milk_available = get_variables(coffee_choice, available_resources)
 
     available_resources["water"] = water_available - water_needed
     available_resources["coffee"] = coffee_available - coffee_needed
@@ -42,43 +53,47 @@ def sum_money(quarters, dimes, nickels, pennies):
     return q_money_inserted + d_money_inserted + n_money_inserted + p_money_inserted
 
 # logic starts
-print(available_resources)
-
+is_on = True
 available_options = ["espresso", "latte", "cappuccino", "off", "report"]
-choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
-while True:
-    if choice not in available_options:
-        print("Please select one of the available choices.")
+
+while is_on:
+    while True:
         choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+        if choice in available_options:
+            break
+        
+        print("Please select one of the available choices.")
+
+    if choice == "off":
+        print("Machine turning off...")
+        is_on = False
+    elif choice == "report":
+        print(available_resources)
     else:
-        break
+        selected_drink = coffee_options[choice]
+        has_resources, message = check_resources(selected_drink, available_resources)
+        if has_resources:
+            quarters = int(input("Insert total quarters: "))
+            nickels = int(input("Insert total nickels: "))
+            dimes = int(input("Insert total dimes: "))
+            pennies = int(input("Insert total pennies: "))
+            money_inserted = sum_money(quarters, nickels, dimes, pennies)
 
-if choice == "off":
-    print("Machine turning off...")
-else:
-    has_resources = check_resources(coffee_options[choice], available_resources)
-    if has_resources:
-        quarters = int(input("Insert total quarters: "))
-        nickels = int(input("Insert total nickels: "))
-        dimes = int(input("Insert total dimes: "))
-        pennies = int(input("Insert total pennies: "))
-        money_inserted = sum_money(quarters, nickels, dimes, pennies)
-        # print(round(money_inserted, 2))
-
-        coffee_price = coffee_options[choice]["money"]
-        if coffee_price > money_inserted:
-            print(f"Insufficient funds. Coffee is ${coffee_price}. You inserted ${round(money_inserted, 2)}.")
-        else:
-            print(f"""Coffee will be ready soon.
+            coffee_price = selected_drink["money"]
+            if coffee_price > money_inserted:
+                print(f"Insufficient funds. Coffee is ${coffee_price}. You inserted ${round(money_inserted, 2)}.")
+            else:
+                print(f"""Your {choice} will be ready soon.
 Printing receipt:
 >> Coffee is ${coffee_price}. 
 >> You inserted ${round(money_inserted, 2)}. 
 >> Your change is ${round(money_inserted - coffee_price, 2)}""")
-            available_resources["money"] += coffee_price
+                available_resources["money"] += coffee_price
 
-            # update resources
-            update_resources(coffee_options[choice], available_resources)
-    else:
-        print("Coffee option not available at the moment.")
-
-print(available_resources)
+                # update resources
+                update_resources(selected_drink, available_resources)
+        else:
+            print(f"Coffee option not available at the moment.")
+            for item in message:
+                print(item)
