14b101c0a6cd693b9e836a6e15eec795d76bbb05
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-06-14 19:36:48 -0600

    duplicated folder deleted

--- flashcard-app/main.py
+++ flashcard-app/main.py
@@ -1,4 +1,20 @@
 # classes
 class AddFlashcard:
-    def __init__(self, flashcard_text):
-        pass
+    def __init__(self, flashcard_question, flashcard_answer):
+        self.flashcard_question = flashcard_question
+        self.flashcard_answer = flashcard_answer
+
+# start program
+flashcards = []
+def start_app(flashcards):
+    print("Welcome back!")
+    option = input("Type STUDY to practice or ADD to add a new flashcard to your deck: ").lower()
+    if option == "add":
+        question = input("Type the question: ")
+        answer = input("Type the answer: ")
+        new_flashcard = AddFlashcard(question, answer)
+        flashcards.append(new_flashcard)
+    else:
+        print("Let's study!")
+        
+start_app(flashcards)
