15e0b32711f679a537df10f22eeb995c34f1b633
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-06-16 12:24:43 -0600

    Day 2: added a loop to go back to the main menu and add cards

--- flashcard-app/main.py
+++ flashcard-app/main.py
@@ -1,20 +1,28 @@
 # classes
-class AddFlashcard:
+class Flashcard:
     def __init__(self, flashcard_question, flashcard_answer):
         self.flashcard_question = flashcard_question
         self.flashcard_answer = flashcard_answer
 
+class Flashcards:
+    def __init__(self, flashcards):
+        self.flashcards = flashcards
+
+
+
 # start program
 flashcards = []
 def start_app(flashcards):
-    print("Welcome back!")
-    option = input("Type STUDY to practice or ADD to add a new flashcard to your deck: ").lower()
-    if option == "add":
-        question = input("Type the question: ")
-        answer = input("Type the answer: ")
-        new_flashcard = AddFlashcard(question, answer)
-        flashcards.append(new_flashcard)
-    else:
-        print("Let's study!")
-        
+    while True:
+        option = input("What do you want to do? PRACTICE (type P) or ADD FLASHCARD (type a): ").lower()
+        if option == "a":
+            while True:
+                question = input("Type the question: ")
+                answer = input("Type the answer: ")
+                flashcard = Flashcard(question, answer)
+                flashcards.append(flashcard)
+                add_card = input("Add another card? Yes (type y) or No (type N): ").lower()
+                if add_card != "y":
+                    break
+
 start_app(flashcards)
