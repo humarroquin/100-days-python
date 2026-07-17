e782492cec4de579803e0b1dc749591cfaec82df
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-07-17 12:24:58 -0600

    refactored the flashcard class and deck class

--- flashcard-app/main.py
+++ flashcard-app/main.py
@@ -8,18 +8,18 @@ class Flashcard:
     def check_answer(self, user_answer):
         return self.answer == user_answer
 
-class Flashcards:
+class Deck:
     def __init__(self):
-        self.flashcards = []
+        self.cards = []
 
     def add_flashcard(self, question, answer):
         flashcard = Flashcard(question, answer)
-        self.flashcards.append(flashcard)
+        self.cards.append(flashcard)
 
-    def save_flashcards(self):
+    def save_deck(self):
         file_path = "saved-cards.txt"
         with open(file_path, "w") as file:
-            for flashcard in self.flashcards:
+            for flashcard in self.cards:
                 line = f"{flashcard.question} | {flashcard.answer}"
                 file.write(line + "\n")
 
@@ -33,13 +33,13 @@ class Flashcards:
                     answer = answer.strip()
                     self.add_flashcard(question, answer)
         except FileNotFoundError:
-            self.flashcards = []
+            print("No saved deck found. Starting with an empty deck.")
 
-    def study_flashcards(self):
+    def study_deck(self):
         score = 0
         q_num = 0
 
-        new_deck = self.flashcards.copy()
+        new_deck = self.cards.copy()
         random.shuffle(new_deck)
 
         for flashcard in new_deck:
@@ -58,7 +58,7 @@ class Flashcards:
         print(f"Your final score is {score}/{len(new_deck)}")
 
 # start program
-deck = Flashcards()
+deck = Deck()
 deck.load_file()
 def start_app(deck):
     while True:
@@ -69,14 +69,14 @@ def start_app(deck):
                 answer = input("Type the answer: ")
 
                 deck.add_flashcard(question, answer)
-                deck.save_flashcards()
+                deck.save_deck()
 
                 add_card = input("Add another card? Yes (type y) or No (type N): ").lower()
                 if add_card != "y":
                     break
         elif option == "s":
-            if deck.flashcards:
-                deck.study_flashcards()
+            if deck.cards:
+                deck.study_deck()
             else:
                 print("No flashcards in deck.")
         elif option == "q":
